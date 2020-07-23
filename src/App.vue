159ddc99cd4556d774e0a57172b30e7b0001d900
101arrowz <arjunbarrett@gmail.com>
2020-07-23 11:09:03 -0700

    bug

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div :class="$style.amazinq">Hello {{ vue }}!</div>
+</template>
+<script lang="ts">
+export default {
+    data() {
+        return {
+            vue: 'Vue'
+        }
+    }
+}
+</script>
+<style module>
+.amazinq {
+    color: red;
+}
+</style>
